62806f579cf3e29a50a2b7ceb149dc2a555fdae2
Kevin Ferrell <kevin.ferrell@capitalone.com>
2017-09-03 17:10:42 -0400

    Update SwiftOCR for for Swift 3.2

--- Pods/Target Support Files/Pods-RoomFinderPro/Pods-RoomFinderPro-frameworks.sh
+++ Pods/Target Support Files/Pods-RoomFinderPro/Pods-RoomFinderPro-frameworks.sh
@@ -90,9 +90,13 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/Eureka/Eureka.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/GPUImage/GPUImage.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/SwiftOCR/SwiftOCR.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/Eureka/Eureka.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/GPUImage/GPUImage.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/SwiftOCR/SwiftOCR.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
