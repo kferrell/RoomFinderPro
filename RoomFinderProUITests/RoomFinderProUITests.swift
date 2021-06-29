977aebeef46b53d7a3e147fa1f2eb0e0e5591140
kevin-j-ferrell <kferrell@apple.com>
2021-06-29 14:57:24 -0400

    Disable performance test

--- RoomFinderProUITests/RoomFinderProUITests.swift
+++ RoomFinderProUITests/RoomFinderProUITests.swift
@@ -31,12 +31,12 @@ class RoomFinderProUITests: XCTestCase {
         XCTAssertTrue(app.staticTexts["Reservations"].exists)
     }
 
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
-    }
+//    func testLaunchPerformance() throws {
+//        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
+//            // This measures how long it takes to launch your application.
+//            measure(metrics: [XCTApplicationLaunchMetric()]) {
+//                XCUIApplication().launch()
+//            }
+//        }
+//    }
 }
